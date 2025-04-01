f18c9b9a863c11abb1012b0b127e80b51bd0d8ab
Sina Moradbakhti <sina.moradbakhti@gmail.com>
2025-04-01 15:25:25 +0200

    check dupplicate for endpoint requestTestAccount

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { RequestIosAccess, RequestIosAccessDocument } from './shared/schemas/request_ios_access.schema';
@@ -16,6 +16,16 @@ export class AppService {
   }
 
   async requestTestAccount(createReqIosAccessDto: CreateReqIosAccessDto) {
+    if (await this.requestIosAccessModel.findOne({ email: createReqIosAccessDto.email })) {
+      throw new HttpException(
+        {
+          status: HttpStatus.BAD_REQUEST,
+          error: 'Already Requested',
+          message: 'You cannot report more than one',
+        },
+        HttpStatus.BAD_REQUEST,
+      );
+    }
     const requestIosAccess = new this.requestIosAccessModel(createReqIosAccessDto);
     return requestIosAccess.save();
   }
