b3232550986de4d678bc21b7085a9fb3ad072336
Sina Moradbakhti <sina.moradbakhti@gmail.com>
2025-04-02 09:36:56 +0200

    updated max chars on create surevey dto (max. 1000)

--- src/shared/dtos/create-survey.dto.ts
+++ src/shared/dtos/create-survey.dto.ts
@@ -1,55 +1,65 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsOptional, IsString, MaxLength } from 'class-validator';
 
 export class CreateSurveyDto {
     @ApiProperty()
     @IsNotEmpty()
     @IsString()
+    @MaxLength(1000)
     readonly q1: string;
 
     @ApiProperty()
     @IsNotEmpty()
     @IsString()
+    @MaxLength(1000)
     readonly q2: string;
 
     @ApiProperty()
     @IsNotEmpty()
     @IsString()
+    @MaxLength(1000)
     readonly q3: string;
 
     @ApiProperty()
     @IsOptional()
     @IsString()
+    @MaxLength(1000)
     readonly q4: string;
 
     @ApiProperty()
     @IsOptional()
     @IsString()
+    @MaxLength(1000)
     readonly q5: string;
 
     @ApiProperty()
     @IsOptional()
     @IsString()
+    @MaxLength(1000)
     readonly q6: string;
 
     @ApiProperty()
     @IsOptional()
     @IsString()
+    @MaxLength(1000)
     readonly q7: string;
 
     @ApiProperty()
     @IsOptional()
     @IsString()
+    @MaxLength(1000)
     readonly q8: string;
 
     @ApiProperty()
     @IsOptional()
     @IsString()
+    @MaxLength(1000)
     readonly q9: string;
 
     @ApiProperty()
     @IsOptional()
     @IsString()
+    @MaxLength(1000)
     readonly q10: string;
 
     @ApiProperty()
