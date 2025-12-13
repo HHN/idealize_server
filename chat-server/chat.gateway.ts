009600ce15cb83620728b976292c3c5260e4491a
David Flaig <david.flaig@hs-heilbronn.de>
2025-12-14 00:53:04 +0100

    edit

--- chat-server/chat.gateway.ts
+++ chat-server/chat.gateway.ts
@@ -31,6 +31,7 @@ export class ChatGateway
     ): Promise<void> {
         try {
             this.logger.log(`Received message from ${client.id}: "${payload.message}"`);
+            this.logger.log(`Payload: "${payload}"`);
             // The user information should already be available in the request due to the guard
             const user = client.handshake.auth.user; // Assuming the user data is attached by the guard
 
