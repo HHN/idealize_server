69226233c17a2fee95661670bba205fd67de8aa3
daflaig <david.flaig@hs-heilbronn.de>
2025-12-16 08:30:20 +0100

    edit

--- src/evaluation/evaluation.service.ts
+++ src/evaluation/evaluation.service.ts
@@ -6,7 +6,7 @@ import * as path from "path";
 export class EvaluationService {
   private readonly jsonFilePath = path.join(
     process.cwd(),
-    "evaluation-data.json"
+    "evaluation-data-test.json"
   );
 
   /**
